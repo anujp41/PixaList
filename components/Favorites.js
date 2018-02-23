ea2e5adb03f7018550188a7d31575bb1808a0fae
Anuj Pant <anuj.pant41@gmail.com>
2018-02-23 16:14:12 +0545

    single card created to display favorites

--- components/Favorites.js
+++ components/Favorites.js
@@ -4,22 +4,39 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  TouchableHighlight
 } from 'react-native';
 import { connect } from 'react-redux';
+import { Card, Icon } from 'react-native-elements'
 
 class Favorites extends Component {
 
-  constructor() {
-    super()
-  }
-
   render() {
+    const favorites = this.props.favorites;
+    const item = favorites['384'];
     return (
-      <View >
-        <Text>All your favorites in one place!</Text>
+        <View >
+          {item 
+          ?
+          <Card
+          title='HELLO WORLD'
+          image={{uri:item.webformatURL}}>
+          <TouchableHighlight onPress={() => console.log('to be removed')} underlayColor='#ed3d3d' style={styles.icon}>
+            <Icon name='favorite' color='red' />
+          </TouchableHighlight>
+          <Text style={{marginBottom: 10}}>
+            <Text style={{fontWeight: 'bold'}}>Uploaded by: </Text><Text>{item.user}</Text>{'\n'}{'\n'}
+            <Text style={{fontWeight: 'bold'}}>Tags: </Text><Text>{item.tags}</Text>{'\n'}{'\n'}
+            <Text style={{fontWeight: 'bold'}}>Resolution: </Text><Text>{item.webformatWidth} X {item.webformatHeight}</Text>
+          </Text>
+        </Card>
+          :
+          <Text>All your favorites in one place!</Text>
+          }
       </View>
-    );
+    )
   }
 }
 
@@ -40,3 +57,12 @@ const mapState = state => {
 
 const FavoritesContainer = connect(mapState, null)(Favorites);
 export default FavoritesContainer;
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 25,
+    height:25,
+    flexWrap: 'nowrap',
+    marginBottom: 10
+  }
+})
